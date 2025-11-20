1771fd5f12ce619e76981dbdbceeaabf5453ae38
Sibgat <sibgatofficial@gmail.com>
2025-11-20 15:30:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let container = document.querySelector(".container");
+let i, j;
+let p = 100;
+for (i = 0; i < p; i++) {
+  for (j = 0; j < p; j++) {
+    let pixel = document.createElement("div");
+    pixel.setAttribute("class", "pixel");
+    container.appendChild(pixel);
+    pixel.style.width = window.innerWidth / p + "px";
+    pixel.style.height = window.innerWidth / p + "px";
+    pixel.onmouseover = () => {
+      pixel.style.background = `rgb(
+  ${Math.floor(Math.random() * 255)},
+  ${Math.floor(Math.random() * 255)},
+  ${Math.floor(Math.random() * 255)}
+)`;
+    };
+  }
+}
